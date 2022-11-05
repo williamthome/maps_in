b75b77684bc16acd4539e0462b6ac337c6e4dbfc
williamthome <willegp88@gmail.com>
2022-11-05 14:01:04 -0300

    refactor: remove the _in from fun names

--- src/maps_in.erl
+++ src/maps_in.erl
@@ -1,65 +1,65 @@
 -module(maps_in).
 
--export([get_in/2, get_in/3, put_in/3, update_in/3, get_and_update_in/3]).
+-export([get/2, get/3, put/3, update/3, get_and_update/3]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
--spec get_in(Path, Map) -> Value when
+-spec get(Path, Map) -> Value when
     Path :: [term()],
     Map :: map(),
     Value :: term().
 
-get_in([Key], Map) ->
+get([Key], Map) ->
     maps:get(Key, Map);
-get_in([Key | Path], Map) ->
-    get_in(Path, maps:get(Key, Map)).
+get([Key | Path], Map) ->
+    get(Path, maps:get(Key, Map)).
 
--spec get_in(Path, Map, Default) -> Value | Default when
+-spec get(Path, Map, Default) -> Value | Default when
     Path :: [term()],
     Map :: map(),
     Default :: term(),
     Value :: term().
 
-get_in([Key], Map, Default) ->
+get([Key], Map, Default) ->
     maps:get(Key, Map, Default);
-get_in([Key | Path], Map, Default) ->
-    get_in(Path, maps:get(Key, Map), Default).
+get([Key | Path], Map, Default) ->
+    get(Path, maps:get(Key, Map), Default).
 
--spec put_in(Path, Value, Map1) -> Map2 when
+-spec put(Path, Value, Map1) -> Map2 when
     Path :: [term()],
     Value :: term(),
     Map1 :: map(),
     Map2 :: map().
 
-put_in([Key], Value, Map) ->
+put([Key], Value, Map) ->
     maps:put(Key, Value, Map);
-put_in([Key | Path], Value, Map) ->
-    put_in(Path, Value, maps:get(Key, Map)).
+put([Key | Path], Value, Map) ->
+    put(Path, Value, maps:get(Key, Map)).
 
--spec update_in(Path, Value, Map1) -> Map2 when
+-spec update(Path, Value, Map1) -> Map2 when
     Path :: [term()],
     Value :: term(),
     Map1 :: map(),
     Map2 :: map().
 
-update_in([Key], Value, Map) ->
+update([Key], Value, Map) ->
     maps:update(Key, Value, Map);
-update_in([Key | Path], Value, Map) ->
-    update_in(Path, Value, maps:get(Key, Map)).
+update([Key | Path], Value, Map) ->
+    update(Path, Value, maps:get(Key, Map)).
 
--spec get_and_update_in(Path, Fun, Map1) -> Map2 when
+-spec get_and_update(Path, Fun, Map1) -> Map2 when
     Path :: [term()],
     Fun :: fun((term()) -> term()),
     Map1 :: map(),
     Map2 :: map().
 
-get_and_update_in([Key], Fun, Map) when is_function(Fun, 1) ->
+get_and_update([Key], Fun, Map) when is_function(Fun, 1) ->
     Value = maps:get(Key, Map),
     maps:update(Key, Fun(Value), Map);
-get_and_update_in([Key | Path], Fun, Map) ->
-    get_and_update_in(Path, Fun, maps:get(Key, Map)).
+get_and_update([Key | Path], Fun, Map) ->
+    get_and_update(Path, Fun, maps:get(Key, Map)).
 
 -ifdef(TEST).
 
@@ -68,41 +68,41 @@ the_movie() ->
       robert => #{name => "Robert Virding", msg => "Hello, Mike"},
       mike => #{msg => "Hello, Robert and Joe"}}.
 
-get_in_2_test() ->
+get_2_test() ->
     [?assertEqual(#{name => "Joe Armstrong", msg => "Hello, Robert"},
-                  get_in([joe], the_movie())),
+                  get([joe], the_movie())),
      ?assertEqual("Hello, Mike",
-                  get_in([robert, msg], the_movie())),
+                  get([robert, msg], the_movie())),
      ?assertError({badkey, name},
-                  get_in([mike, name], the_movie())),
+                  get([mike, name], the_movie())),
      ?assertError({badmap, "Hello, Robert and Joe"},
-                  get_in([mike, msg, hello], the_movie()))].
+                  get([mike, msg, hello], the_movie()))].
 
-get_in_3_test() ->
+get_3_test() ->
     [?assertEqual("Hello, Mike",
-                  get_in([robert, msg], the_movie(), "Hello, Joe")),
+                  get([robert, msg], the_movie(), "Hello, Joe")),
      ?assertEqual("Mike Williams",
-                  get_in([mike, name], the_movie(), "Mike Williams"))].
+                  get([mike, name], the_movie(), "Mike Williams"))].
 
-put_in_test() ->
+put_test() ->
     [?assertEqual(#{name => "Mike Williams", msg => "Hello, Robert and Joe"},
-                  put_in([mike, name], "Mike Williams", the_movie())),
+                  put([mike, name], "Mike Williams", the_movie())),
      ?assertEqual(#{erlang => "The Movie"},
-                  put_in([erlang], "The Movie", #{}))].
+                  put([erlang], "The Movie", #{}))].
 
-update_in_test() ->
+update_test() ->
     [?assertError({badkey, erlang},
-                  update_in([erlang], "The Movie", #{})),
+                  update([erlang], "The Movie", #{})),
      ?assertEqual(#{erlang => "The Movie"},
-                  update_in([erlang], "The Movie", #{erlang => ""}))].
+                  update([erlang], "The Movie", #{erlang => ""}))].
 
-get_and_update_in_test() ->
+get_and_update_test() ->
     [?assertError({badkey, erlang},
-                  get_and_update_in([erlang], "The Movie", #{})),
+                  get_and_update([erlang], "The Movie", #{})),
      ?assertError(function_clause,
-                  get_and_update_in([erlang], "The Movie", #{erlang => ""})),
+                  get_and_update([erlang], "The Movie", #{erlang => ""})),
      ?assertEqual(#{erlang => "The Movie"},
-                  get_and_update_in([erlang], fun("") -> "The Movie" end,
+                  get_and_update([erlang], fun("") -> "The Movie" end,
                                     #{erlang => ""}))].
 
 -endif.
